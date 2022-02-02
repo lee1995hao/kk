b162a7776ec3d729e39cfaee0331f619ab82c186
lee1995hao <85012206+lee1995hao@users.noreply.github.com>
2022-02-02 14:09:18 +0900

    how to calculate integral monte carol method

--- Monte Carlo.py
+++ Monte Carlo.py
@@ -8,8 +8,7 @@ def mc(n):
     x = np.random.uniform(0,1,n)
     y = np.random.uniform(0,1,n)# make the point with y for each x point
     for i in range(n):
-        if y[i] < lin(x[i]): ##for x = x if y(x)< f(x) then the point is under the line use all point/1 = the point under the line/
-integral area
+        if y[i] < lin(x[i]): ##for x = x if y(x)< f(x) then the point is under the line use all point/1 = the point under the line/integral area
             k = k+1
     k = np.array(k)
     return k/n
